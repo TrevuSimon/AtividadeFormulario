d169fb0de56177e64986996e9975ab08de8489de
Guilherme Simao <guilherme.1301703@fapi-pinhais.edu.br>
2018-08-31 20:33:43 -0300

    Trabalhando no formulario

---
+++ CheckBox/Pages/Check.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CheckBox.Pages
+{
+    public class CheckModel : PageModel
+    {
+
+        public List<String> Cor { get; set; } = new List<String>();
+        public List<String> Sexo { get; set; } = new List<String>();
+        public Dictionary<String,String> Escolhas { get; set; } = new Dictionary<String,String>();
+
+        public CheckModel()
+        {
+            Cor.Add("Caucassiano");
+            Cor.Add("Preto");
+            Cor.Add("Pardo");
+            Cor.Add("Indigena");
+
+            Sexo.Add("horme");
+            Sexo.Add("Mulher");
+
+            Escolhas.Add("Int","Inteligente");
+            Escolhas.Add("Bol","Come bolacha");
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
