c86732b3d2f3297b51b79fc957bc6afab313f657
Guilherme Simao <guilherme.1301703@fapi-pinhais.edu.br>
2018-08-31 21:09:30 -0300

    corrigindo bugs

--- CheckBox/Pages/Result.cshtml.cs
+++ CheckBox/Pages/Result.cshtml.cs
@@ -12,13 +12,13 @@ namespace CheckBox.Pages
         public String Nome { get; set; }
         public List<String> Cor { get; set; } = new List<String>();
         public List<String> Sexo { get; set; } = new List<String>();
-        public Dictionary<String, String> Escolhas { get; set; } = new Dictionary<String, String>();
+        public List<String> Escolhas { get; set; } = new List< String>();
 
         public void OnGet(
             String Nome,
             List<String> Cor,
             List<String> Sexo,
-            Dictionary<String, String> Escolhas)
+            List<String> Escolhas)
         {
             this.Nome = Nome;
             this.Cor = Cor;
